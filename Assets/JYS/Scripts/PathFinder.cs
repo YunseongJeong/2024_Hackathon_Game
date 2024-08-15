7b3423a15d584ac35444290eb41562617f4885bf
mandu <jys0972@gmail.com>
2024-08-15 12:05:11 +0900

    build setting

--- Assets/JYS/Scripts/PathFinder.cs
+++ Assets/JYS/Scripts/PathFinder.cs
@@ -1,12 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace PathFinder
 {
